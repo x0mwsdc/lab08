633d659288f1facfcd48f3c9cc82ab3fee0adc95
x0mwsdc <danilagl_n@mail.ru>
2026-06-10 00:57:46 +0300

    fixed build paths and successfully ran tests

---
+++ tests/test1.cpp
@@ -0,0 +1,17 @@
+#include <print.hpp>
+#include <gtest/gtest.h>
+#include <fstream>
+#include <string>
+
+TEST(Print, InFileStream) {
+    std::string filepath = "file.txt";
+    std::string text = "hello";
+    std::ofstream out{filepath};
+    print(text, out);
+    out.close();
+
+    std::string result;
+    std::ifstream in{filepath};
+    in >> result;
+    EXPECT_EQ(result, text);
+}
