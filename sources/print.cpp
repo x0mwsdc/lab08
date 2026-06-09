1bda752f765bd0b58ab8841baff7a0f079e5ba08
x0mwsdc <danilagl_n@mail.ru>
2026-06-10 01:58:04 +0300

    fix: add explicit includes for std::string in print source

--- sources/print.cpp
+++ sources/print.cpp
@@ -1,7 +1,7 @@
 #include <print.hpp>
+#include <string>
+#include <ostream>
+
 void print(const std::string& text, std::ostream& out) {
-  out << text;
-}
-void print(const std::string& text, std::ofstream& out) {
-  out << text;
+    out << text;
 }
